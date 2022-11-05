db5dfba85dbfc3dc9f136464888bbe08cb693be4
biggirl <oncwnuAmbli3YDdfEaxJTsJEJZMI@git.weixin.qq.com>
2022-11-06 00:26:27 +0800

    斐波

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -2,53 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sort"
 )
 
-type Student struct {
-	Name  string
-	Age   int
-	Score float64
-}
-
-type StuSlice []Student
-
-func (s StuSlice) Len() int {
-	// sort.Sort()
-	// fmt.Println("say()")
-	return len(s)
-}
-func (s StuSlice) Less(i, j int) bool {
-	// sort.Sort()
-	// fmt.Println("say()")
-	return true
-}
-func (s StuSlice) Swap(i, j int) {
-	// sort.Sort()
-	// fmt.Println("say()")
-	//return true
+//递归法
+func fbn1(n int) int {
+	if n == 1 || n == 2 {
+		return 1
+	} else {
+		return fbn1(n-1) + fbn1(n-2)
+	}
 }
 
 func main() {
-	var stus StuSlice
-	for i := 0; i <= 10; i++ {
-		stu := Student{
-			Name:  fmt.Sprintf("英雄|%d", rand.Intn(100)),
-			Age:   rand.Intn(100),
-			Score: rand.Float64(),
-		}
-		stus = append(stus, stu)
-	}
 
-	for _, v := range stus {
-		fmt.Println(v)
-	}
-	fmt.Println("-----------排序后------------")
+	res := fbn1(3)
+	fmt.Println("res=", res)
 
-	sort.Sort(stus)
-	for _, v := range stus {
-		fmt.Println(v)
-	}
-
+	fmt.Println("res=", fbn1(6))
 }
